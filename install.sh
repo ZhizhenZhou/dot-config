3936ea846568dbd09d11463fc071526828293564
ZhizhenZhou <53110831+ZhizhenZhou@users.noreply.github.com>
2024-09-10 17:12:49 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -10,89 +10,48 @@ command_exists() {
   command -v "$@" >/dev/null 2>&1
 }
 
-# if command_exists zsh; then
-#     if [ "${SHELL#*zsh}" != "$SHELL" ]; then
-#         echo "zsh is default shell."
-#         zsh_version=$(zsh --version | awk '{print $2}')
-#         required_version="5.0.8"
-#         if [ "$(printf '%s\n' "$required_version" "$zsh_version" | sort -V | head -n1)" = "$required_version" ]; then
-#             if ! command_exists omz; then
-#                 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#             
-#         else
-#             print_error "you have \`zsh $zsh_version\` installed. But \`oh-my-zsh\` needs \`zsh 5.0.8\` or more recent."
-#         fi
-#     else
-#         chsh -s $(which zsh)
-#     fi
-#
-#     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-#     git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-#
-#     # 安装自定义的zsh主题
-#     curl -o ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/zzz.zsh-theme https://raw.githubusercontent.com/ZhizhenZhou/zsh-config/main/zzz.zsh-theme
-#
-#     sed -i 's/ZSH_THEME=".*"/ZSH_THEME="zzz"/g' ${HOME:-~}/.zshrc
-#     sed -i 's/plugins=(.*)/plugins=(zsh-syntax-highlighting zsh-autosuggestions z git)/g' ${HOME:-~}/.zshrc
-#
-#     {
-#         echo "alias vim='nvim'"
-#         echo "alias vi='nvim'"
-#
-#         echo "export EDITOR='nvim'"
-#         echo "export VISUAL='nvim'"
-#     } >> ${HOME:-~}/.zshrc
-#
-#
-#     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
-#
-#     source ${HOME:-~}/.zshrc
-#
-#     nvm install 20
-#
-#     echo "脚本执行完成。请重新登录以应用更改。"
-# else
-#     print_error "zsh is not installed."
-# fi
-
 if [ "${SHELL#*zsh}" != "$SHELL" ]; then
-# if [[ "$SHELL" == *"/zsh" ]]; then
-    if [ ! -d "$HOME/.oh-my-zsh" ]; then
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || {
-            echo "ohmyzsh install fail."
-            exit 1
-        }
-    else 
-        echo "you have already installed ohmyzsh."
-        exit 1
+    if command_exists omz; then
+        if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]; then
+            git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+        else
+            rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+            git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+        fi
+
+        if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]; then
+            git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+        else
+            rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+            git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+        fi
+
+        # 安装自定义的zsh主题
+        if [ ! -f ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/zzz.zsh-theme ]; then
+            curl -o ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/zzz.zsh-theme https://raw.githubusercontent.com/ZhizhenZhou/zsh-config/main/zzz.zsh-theme
+        else
+            rm -f ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/zzz.zsh-theme
+            curl -o ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/zzz.zsh-theme https://raw.githubusercontent.com/ZhizhenZhou/zsh-config/main/zzz.zsh-theme
+        fi
+
+        sed -i 's/ZSH_THEME=".*"/ZSH_THEME="zzz"/g' ${HOME:-~}/.zshrc
+        sed -i 's/plugins=(.*)/plugins=(zsh-syntax-highlighting zsh-autosuggestions z git)/g' ${HOME:-~}/.zshrc
+
+        # 这里应该放到neovim的配置里
+        # {
+        #     echo "alias vim='nvim'"
+        #     echo "alias vi='nvim'"
+        #
+        #     echo "export EDITOR='nvim'"
+        #     echo "export VISUAL='nvim'"
+        # } >> ${HOME:-~}/.zshrc
+    else
+        echo "please install oh-my-zsh first."
     fi
 else
-    echo "zsh is not your default shell."
+    echo "please set zsh your default shell."
     exit 1
 fi
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-# 安装自定义的zsh主题
-curl -o ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/zzz.zsh-theme https://raw.githubusercontent.com/ZhizhenZhou/zsh-config/main/zzz.zsh-theme
-
-sed -i 's/ZSH_THEME=".*"/ZSH_THEME="zzz"/g' ${HOME:-~}/.zshrc
-sed -i 's/plugins=(.*)/plugins=(zsh-syntax-highlighting zsh-autosuggestions z git)/g' ${HOME:-~}/.zshrc
-
-{
-    echo "alias vim='nvim'"
-    echo "alias vi='nvim'"
-
-    echo "export EDITOR='nvim'"
-    echo "export VISUAL='nvim'"
-} >> ${HOME:-~}/.zshrc
-
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
-
 source ${HOME:-~}/.zshrc
 
-nvm install 20
-
-echo "脚本执行完成。请重新登录以应用更改。"
